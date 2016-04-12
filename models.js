3936c78e81c57423bc914b514dfc355bac878ab3
Calvin Gomez <calvin.gomez1@gmail.com>
2016-04-11 22:42:09 -0700

    Added data models

--- models.js
+++ models.js
@@ -1,12 +1,45 @@
 var mongoose = require('mongoose');
 
-var twitterAuthSchema = new mongoose.Schema({
+var twitterUserSchema = new mongoose.Schema({
 	"twitterID": String,
     "token": String,
     "username": String,
     "displayName": String,
     "photos": [ {"value": String } ]
 
+}, {
+	collection: "userdata"
 });
 
-exports.User = mongoose.model('twitterUser',twitterAuthSchema);
+var courseSchema = new mongoose.Schema({
+	"name": String,
+    "prof": String,
+    "info": String,
+    "number": Number
+
+}, {
+	collection: "courses"
+});
+
+var commentSchema = new mongoose.Schema({
+	"username": String,
+    "course_id": String,
+    "text": String,
+    "timestamp": { type: Date, default: Date.now }
+
+}, {
+	collection: "comments"
+});
+
+var courseRatingSchema = new mongoose.Schema({
+    "course_id": String,
+    "rating": String
+
+}, {
+	collection: "overallratings"
+});
+
+exports.User = mongoose.model('twitterUser',twitterUserSchema);
+exports.Course = mongoose.model('courseDetail',courseSchema);
+exports.Comment = mongoose.model('commentDetail',commentSchema);
+exports.OverallRating = mongoose.model('overallCourseRating',courseRatingSchema);
